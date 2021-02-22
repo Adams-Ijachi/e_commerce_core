395ae976498538200545a70706b9b0de7a67175a
Adams Ijachi <adamsijachi9@gmail.com>
2021-02-22 23:15:38 +0100

    minor change

--- com/settings.py
+++ com/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
 
 AUTH_USER_MODEL = 'accounts.User'
 
-LOGIN_URL = 'login'
+LOGIN_URL = '/accounts/login'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
